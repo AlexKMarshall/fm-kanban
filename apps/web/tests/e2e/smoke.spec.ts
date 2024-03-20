b95ab5ed6a17e679c72d8569029dff1d1c1eeb73
AlexKMarshall <alexmarshall@qogita.com>
2024-03-20 15:38:55 +0000

    chore: remove dupes

--- apps/web/tests/e2e/smoke.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test'
-
-test('App loads', async ({ page }) => {
-  await page.goto('/')
-
-  await expect(page.getByRole('heading', { name: /kanban app/i })).toBeVisible()
-})
