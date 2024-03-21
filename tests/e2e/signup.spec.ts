7d026e6f8cd09deb9703aa7b80997d20eeffa3cd
Alex Marshall <alex.k.marshall83@gmail.com>
2024-03-21 09:07:51 +0000

    Test signin (#68)
    * ci: add db push step to script
    
    * test: sign up flow
    
    * ci: add database url to env
    
    * test: only run on chrome

---
+++ tests/e2e/signup.spec.ts
@@ -0,0 +1,28 @@
+import { faker } from '@faker-js/faker'
+import { expect, test } from '@playwright/test'
+
+type User = {
+  email: string
+  password: string
+}
+function makeUser(overrides?: Partial<User>): User {
+  return {
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    ...overrides,
+  }
+}
+
+test('Sign up flow', async ({ page }) => {
+  await page.goto('/')
+  const user = makeUser()
+
+  await page.getByRole('link', { name: /signup/i }).click()
+  await page.getByRole('textbox', { name: /email/i }).fill(user.email)
+  await page.getByRole('textbox', { name: /password/i }).fill(user.password)
+  await page.getByRole('button', { name: /sign up/i }).click()
+
+  await expect(
+    page.getByRole('button', { name: /create new board/i }),
+  ).toBeVisible()
+})
