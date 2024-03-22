d83e8ccb7020a19e90b7955c1e533161537c6b01
AlexKMarshall <alexmarshall@qogita.com>
2024-03-22 13:05:41 +0000

    test: board name required

--- tests/e2e/createBoard.spec.ts
+++ tests/e2e/createBoard.spec.ts
@@ -62,3 +62,25 @@ test('create board with no columns', async ({ page, login }) => {
     page.getByRole('heading', { name: boardWithoutColumns.name }),
   ).toBeVisible()
 })
+
+test('board name is required', async ({ page, login }) => {
+  await login()
+
+  await page.goto('/')
+  await page.getByRole('button', { name: /create new board/i }).click()
+  const createBoardForm = page.getByRole('form').filter({
+    has: page.getByRole('button', { name: /create new board/i }),
+  })
+
+  await createBoardForm
+    .getByRole('button', { name: /create new board/i })
+    .click()
+
+  const nameField = createBoardForm.getByRole('textbox', { name: /^name/i })
+  await expect(nameField).toHaveAttribute('aria-invalid', 'true')
+  const describedById = await nameField.getAttribute('aria-describedby')
+  if (!describedById) throw new Error('aria-describedby is not set')
+  const errorMessage = page.locator(`#${describedById}`)
+  await expect(errorMessage).toBeVisible()
+  await expect(errorMessage).toHaveText("Can't be empty")
+})
