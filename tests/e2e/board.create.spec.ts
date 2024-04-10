428fc87fe566d6d3a31ef5894224ac86e3b1683b
AlexKMarshall <alexmarshall@qogita.com>
2024-04-10 08:11:29 +0100

    test: first mobile test

--- tests/e2e/board.create.spec.ts
+++ tests/e2e/board.create.spec.ts
@@ -9,28 +9,28 @@ test('create board', async ({ page, login }) => {
   await page.goto('/')
 
   await page.getByRole('button', { name: /create new board/i }).click()
-  const createBoardForm = page.getByRole('form').filter({
+  const createBoardDialog = page.getByRole('dialog').filter({
     has: page.getByRole('button', { name: /create new board/i }),
   })
 
-  await createBoardForm
+  await createBoardDialog
     .getByRole('textbox', { name: /^name/i })
     .fill(board.name)
 
   for (let i = 0; i < board.columns.length; i++) {
     const column = board.columns[i]
     if (i > 0) {
-      await createBoardForm
+      await createBoardDialog
         .getByRole('button', { name: /add new column/i })
         .click()
     }
-    await createBoardForm
+    await createBoardDialog
       .getByRole('textbox', { name: /column name/i })
       .last()
       .fill(column.name)
   }
 
-  await createBoardForm
+  await createBoardDialog
     .getByRole('button', { name: /create new board/i })
     .click()
 
@@ -46,15 +46,15 @@ test('create board with no columns', async ({ page, login }) => {
 
   await page.goto('/')
   await page.getByRole('button', { name: /create new board/i }).click()
-  const createBoardForm = page.getByRole('form').filter({
+  const createBoardDialog = page.getByRole('dialog').filter({
     has: page.getByRole('button', { name: /create new board/i }),
   })
 
-  await createBoardForm
+  await createBoardDialog
     .getByRole('textbox', { name: /^name/i })
     .fill(boardWithoutColumns.name)
 
-  await createBoardForm
+  await createBoardDialog
     .getByRole('button', { name: /create new board/i })
     .click()
 
@@ -68,15 +68,15 @@ test('board name is required', async ({ page, login }) => {
 
   await page.goto('/')
   await page.getByRole('button', { name: /create new board/i }).click()
-  const createBoardForm = page.getByRole('form').filter({
+  const createBoardDialog = page.getByRole('dialog').filter({
     has: page.getByRole('button', { name: /create new board/i }),
   })
 
-  await createBoardForm
+  await createBoardDialog
     .getByRole('button', { name: /create new board/i })
     .click()
 
-  const nameField = createBoardForm.getByRole('textbox', { name: /^name/i })
+  const nameField = createBoardDialog.getByRole('textbox', { name: /^name/i })
   await expect(nameField).toBeAriaInvalid()
   await expect(nameField).toBeDescribedBy("Can't be empty")
 })
